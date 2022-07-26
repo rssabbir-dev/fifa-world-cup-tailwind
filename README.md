9407d74a97fb45a4cf613ef112cbe361b621f353
rssabbir <sabbirhossainrahad@gmail.com>
2022-07-26 21:08:49 +0600

    blogs page done, made with tailwindCSS

---
+++ README.md
@@ -0,0 +1,2 @@
+# fifa-world-cup - made with tailwind css for practice
+# Live Website Link: https://rssabbir-dev.github.io/fifa-world-cup-tailwind
---
+++ css/tailwindcustomstyle.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				primaryColor: "#0031AA",
+				secondaryColor:"#03012F",
+				grayLight: "#f3f3f373",
+				grayDark: "#f2f1f9",
+				textColor: "#fff",
+			},
+		},
+	},
+};
