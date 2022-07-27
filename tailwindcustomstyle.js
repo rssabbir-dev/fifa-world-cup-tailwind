5dea693df49518578bbd7519c5df67a6afb01911
rssabbir <sabbirhossainrahad@gmail.com>
2022-07-27 15:24:52 +0600

    match and group page done

---
+++ tailwindcustomstyle.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+	theme: {
+		extend: {
+			colors: {
+				primaryColor: "#0031AA",
+				secondaryColor: "#03012F",
+				grayLight: "#f3f3f373",
+				grayDark: "#f2f1f9",
+				textColor: "#fff",
+			},
+			backgroundImage: {
+				bannerLg:
+					"linear-gradient(90deg, rgba(2, 0, 36, 1) 25%, rgba(9, 9, 121, 1) 70%)",
+				banner: "linear-gradient(180deg, rgba(2, 0, 36, 1) 25%, rgba(9, 9, 121, 1) 70%)",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
+	},
+};
+
+// 1023-1280
